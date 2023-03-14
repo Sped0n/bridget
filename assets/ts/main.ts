a647bd5d18b1e8e5dde85bc44fb1d2d8298ad9d8
Spedon <RealSped0n@outlook.com>
2023-03-14 23:31:02 +0800

    add image cache function

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -3,8 +3,10 @@ import { trackMouseInit } from './trackMouse'
 import { thresholdCtlInit } from './thresholdCtl'
 import { imagesArrayLen } from './dataFetch'
 import { vwRefreshInit } from './overlay'
+import { preloader } from './imageCache'
 
 function init(): void {
+  preloader(0)
   vwRefreshInit()
   imgIndexSpanUpdate(0, imagesArrayLen)
   thresholdCtlInit()
