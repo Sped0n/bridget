e47b8806f6114577b3c9c6ec735eb4f9180b6429
Spedon <RealSped0n@outlook.com>
2023-03-20 21:57:51 +0800

    disable preloader

--- assets/ts/imageCache.ts
+++ assets/ts/imageCache.ts
@@ -4,5 +4,15 @@ import { preloadImage, calcImageIndex } from './utils'
 let lastIndex: number = 0
 
 export const preloader = (index: number): void => {
+  if (lastIndex === index) {
+    for (let i: number = -2; i <= 1; i++)
+      preloadImage(imagesArray[calcImageIndex(index + i, imagesArrayLen)].url)
+  } else if (lastIndex > index) {
+    for (let i: number = 1; i <= 3; i++)
+      preloadImage(imagesArray[calcImageIndex(index - i, imagesArrayLen)].url)
+  } else {
+    for (let i: number = 1; i <= 3; i++)
+      preloadImage(imagesArray[calcImageIndex(index + i, imagesArrayLen)].url)
+  }
   lastIndex = index
 }
