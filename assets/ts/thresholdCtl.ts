5e311692f4ad02215567e44ad0c90b06b62eb3d5
Spedon <RealSped0n@outlook.com>
2023-03-28 18:55:53 +0800

    add stack update the refresh components

--- assets/ts/thresholdCtl.ts
+++ assets/ts/thresholdCtl.ts
@@ -1,4 +1,5 @@
 import { duper } from './utils'
+import { setStackDepth } from './desktop'
 
 // get threshold display element
 const thresholdDisp = document.getElementsByClassName('thid').item(0) as HTMLSpanElement
@@ -8,9 +9,15 @@ const threshold: number[] = [0, 40, 80, 120, 160, 200]
 export const thresholdSensitivityArray: number[] = [100, 40, 18, 14, 9, 5]
 export let thresholdIndex: number = 2
 
+export const stackDepthArray: number[] = [15, 8, 5, 5, 5, 5]
+
 // update inner text of threshold display element
 const thresholdUpdate = (): void => {
   thresholdDisp.innerText = duper(threshold[thresholdIndex])
+}
+
+const stackDepthUpdate = (): void => {
+  setStackDepth(stackDepthArray[thresholdIndex])
 }
 
 // threshold control initialization
@@ -23,6 +30,7 @@ export const thresholdCtlInit = (): void => {
       if (thresholdIndex > 0) {
         thresholdIndex--
         thresholdUpdate()
+        stackDepthUpdate()
       }
     },
     { passive: true }
@@ -35,6 +43,7 @@ export const thresholdCtlInit = (): void => {
       if (thresholdIndex < 5) {
         thresholdIndex++
         thresholdUpdate()
+        stackDepthUpdate()
       }
     },
     { passive: true }
