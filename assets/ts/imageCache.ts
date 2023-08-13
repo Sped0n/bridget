262f7f7da82f9807131504fa0185441097dbee61
Sped0n <realsped0n@outlook.com>
2023-08-13 22:33:47 +0800

    refractor: change var name

--- assets/ts/imageCache.ts
+++ assets/ts/imageCache.ts
@@ -1,4 +1,4 @@
-import { imagesArray, imagesArrayLen } from './dataFetch'
+import { imagesInfo, imagesLen } from './dataFetch'
 import { preloadImage, calcImageIndex } from './utils'
 
 let lastIndex: number = 0
@@ -6,13 +6,13 @@ let lastIndex: number = 0
 export const preloader = (index: number): void => {
   if (lastIndex === index) {
     for (let i: number = -2; i <= 1; i++)
-      preloadImage(imagesArray[calcImageIndex(index + i, imagesArrayLen)].url)
+      preloadImage(imagesInfo[calcImageIndex(index + i, imagesLen)].url)
   } else if (lastIndex > index) {
     for (let i: number = 1; i <= 3; i++)
-      preloadImage(imagesArray[calcImageIndex(index - i, imagesArrayLen)].url)
+      preloadImage(imagesInfo[calcImageIndex(index - i, imagesLen)].url)
   } else {
     for (let i: number = 1; i <= 3; i++)
-      preloadImage(imagesArray[calcImageIndex(index + i, imagesArrayLen)].url)
+      preloadImage(imagesInfo[calcImageIndex(index + i, imagesLen)].url)
   }
   lastIndex = index
 }
