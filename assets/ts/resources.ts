3cbbc5b6da17b874552f6199a295709cf90c8767
Sped0n <realsped0n@outlook.com>
2023-10-30 17:44:59 +0800

    refactor(resources.ts): update ImageJSON interface to include separate properties for low resolution and high resolution image URLs, heights, and widths for better organization and clarity
    refactor(imageJSON.html): modify image resizing logic to generate separate low resolution and high resolution image URLs, heights, and widths to improve performance and optimize image loading

--- assets/ts/resources.ts
+++ assets/ts/resources.ts
@@ -1,11 +1,12 @@
 // data structure for images info
 export interface ImageJSON {
   index: number
-  url: string
-  imgH: number
-  imgW: number
-  pColor: string
-  sColor: string
+  loUrl: string
+  loImgH: number
+  loImgW: number
+  hiUrl: string
+  hiImgH: number
+  hiImgW: number
 }
 
 export function initResources(): ImageJSON[] {
