b3a48c96c7fb4aa8523e87328ab2ce6ab71c6f74
Sped0n <realsped0n@outlook.com>
2024-01-20 17:46:14 +0800

    feat: update navigation functionality and threshold rendering

--- assets/ts/nav.ts
+++ assets/ts/nav.ts
@@ -36,9 +36,6 @@ const indexDispNums = Array.from(
  */
 
 export function initNav(): void {
-  const s = state.get()
-  // init threshold text
-  updateThresholdText(expand(s.threshold))
   // add watcher for updating nav text
   state.addWatcher((o) => {
     updateIndexText(expand(o.index + 1), expand(o.length))
