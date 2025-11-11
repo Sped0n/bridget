9e9c8ed175bc07c64c8ec45cac5416ca7f20dda6
Sped0n <hi@sped0n.com>
2025-11-11 16:58:08 +0800

    chore: add nix flake for reproducible dev environment
    Signed-off-by: Sped0n <hi@sped0n.com>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "bridget";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  outputs =
+    { self, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              nodejs
+              nodePackages.pnpm
+              dart-sass
+              hugo
+              go
+            ];
+          };
+        }
+      );
+    };
+}
