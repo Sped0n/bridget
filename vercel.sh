304abf3b65aa6b3c11a0f33fe8159e385a11ac25
Sped0n <hi@sped0n.com>
2025-11-12 04:27:15 +0800

    refactor: switch to sass-embedded and remove manual install
    Signed-off-by: Sped0n <hi@sped0n.com>

--- vercel.sh
+++ vercel.sh
@@ -3,20 +3,6 @@ set -euo pipefail
 
 node_modules_generated_dir="./node_modules/exampleSite/resources/_gen"
 project_generated_dir="./exampleSite/resources/_gen"
-dart_sass_version="1.93.3"
-dart_sass_install_dir="${HOME}/.local/dart-sass"
-dart_sass_tarball="dart-sass-${dart_sass_version}-linux-x64.tar.gz"
-dart_sass_download_url="https://github.com/sass/dart-sass/releases/download/${dart_sass_version}/${dart_sass_tarball}"
-
-install_dart_sass() {
-  echo "Installing Dart Sass ${dart_sass_version}..."
-  mkdir -p "${HOME}/.local"
-  curl -sSLO "${dart_sass_download_url}"
-  rm -rf "${dart_sass_install_dir}"
-  tar -C "${HOME}/.local" -xf "${dart_sass_tarball}"
-  rm -f "${dart_sass_tarball}"
-  export PATH="${dart_sass_install_dir}:${PATH}"
-}
 
 copy_generated_assets_to_project() {
   if [ -d "${node_modules_generated_dir}" ]; then
@@ -44,7 +30,6 @@ copy_generated_assets_to_node_modules() {
   fi
 }
 
-install_dart_sass
 copy_generated_assets_to_project
 run_site_build
 copy_generated_assets_to_node_modules
